8d83a0ad6e11b07d7bba1b733e539b13424bdb59
TaeJun <zmddl741@naver.com>
2019-11-20 16:28:46 +0900

    2D배경 깔기
    배경깔기

---
+++ Assets/Scripts/Bubbles.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubbles : MonoBehaviour
+{
+    [SerializeField] float speed = 0.5f;
+    [SerializeField] float startPosition;
+    [SerializeField] float endPosition;
+    void Update()
+    {
+         transform.Translate(-1 * speed * Time.deltaTime, 0, 0);
+
+        if(transform.position.z <= endPosition)
+        {
+            transform.Translate(-1 * (endPosition - startPosition), 0, 0);
+
+            SendMessage("ChangePosition", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
